e95989073cab7e6fb52f929c30a7160672e0abca
csh1314 <cshsb@qq.com>
2022-04-27 10:18:24 +0800

    fix: tuple to obj

--- challenges/easy/easy-tuple-to-object/template.ts
+++ challenges/easy/easy-tuple-to-object/template.ts
@@ -1,6 +1,14 @@
-type TupleToObject<T extends readonly any[]> = {
-  [K in T[number]] : K
-}
+// type TupleToObject<T extends readonly (keyof any)[]> = {
+//   [K in T[number]] : K
+// }
 
 // 元组转对象
 // 遍历数组:  K in T[number]
+// 此题需要注意 TupleToObject<[[1, 2], {}]> 抛出错误
+// 因此需要限定数组中的每个元素的类型
+
+// 内置类型 type PropertyKey = string | number | symbol  (限定为非引用类型)
+
+type TupleToObject<T extends readonly PropertyKey[]> = {
+  [K in T[number]] : K
+}
