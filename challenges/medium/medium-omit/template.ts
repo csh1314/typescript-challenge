f443d86f73173feaf23d4ac981fd45202607c962
csh1314 <cshsb@qq.com>
2022-04-28 16:38:10 +0800

    fix: omit

--- challenges/medium/medium-omit/template.ts
+++ challenges/medium/medium-omit/template.ts
@@ -1,7 +1,7 @@
-type MyOmit<T, K> = {
- [P in keyof T as Exclude<P, K>] : T[P]
+type MyOmit<T, K extends keyof T> = {
+ [P in Exclude<keyof T, K>] : T[P]
 }
 
 
 // Omit ===> 创建一个省略了K中字段的 T 对象
-// P in keyof T as Exclude<P, K> ===> 在T中的能给K赋值类型的P
+// P in Exclude<keyof T, K> ===> 在T中的能给K赋值类型的P
